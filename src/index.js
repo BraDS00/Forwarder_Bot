4d1b6116d4d0cb95b751118312819a6f5a3ec9b9
Kiarash Madani <77398305+kiarashmadani@users.noreply.github.com>
2026-08-11 08:38:21 +0330

    Modified index.js for port opening

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 require('dotenv').config();
 
+const http = require('http');
 const bot = require('./bot');
+
+const PORT = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Forwarder Bot is running!');
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Web server is running on port ${PORT}`);
+});
 
 async function startBot() {
     await bot.launch();
-    console.log('Forwarder Bot is running. Press Ctrl+C to stop it.');
+
+    console.log('Forwarder Bot is running.');
     console.log('Bot is ready to forward messages.');
-    console.log(req.body);
 }
 
 startBot().catch((error) => {
@@ -14,5 +29,12 @@ startBot().catch((error) => {
     process.exit(1);
 });
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once('SIGINT', () => {
+    bot.stop('SIGINT');
+    server.close();
+});
+
+process.once('SIGTERM', () => {
+    bot.stop('SIGTERM');
+    server.close();
+});
